7ca1d362a528a8b907b31ed2317eb8206f9f09c1
Kassandra <K1995Moreno@gmail.com>
2022-10-22 03:38:30 -0400

    first background draft

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Content added by JavaScript";
 document.querySelector("body").appendChild(h2);
+
+/*Review*/

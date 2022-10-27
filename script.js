72bd4c3bd53c7e77875543f2876113aa85a83aec
Kassandra <K1995Moreno@gmail.com>
2022-10-27 05:12:44 -0400

    pages 1-4 foundational touch ups + links added

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const h2 = document.createElement("h2");
-h2.textContent = "Contents added by JavaScript";
-document.querySelector(body).appendChild(h2);
-
-/*Review*/
+// Figure out
+/*
+const underConstruction= document.createElement("underConstruction")
+underConstruction= images/Patrick.png
+*/ 
